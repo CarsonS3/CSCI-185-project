0bc15221a1d896652f9ff34ec4596f2f1f6b730e
SOUMIK <SOUMIK@LAPTOP-C4VOMURG>
2023-04-24 14:56:09 -0400

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class mainForm
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnScience_Click(sender As Object, e As EventArgs) Handles btnScience.Click
+
+    End Sub
+
+    Private Sub btnEnglish_Click(sender As Object, e As EventArgs) Handles btnEnglish.Click
+
+    End Sub
+End Class
