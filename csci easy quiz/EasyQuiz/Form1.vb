d211e07fd169ae2da2571d919fe008f4d9736074
CarsonS3 <131296237+CarsonS3@users.noreply.github.com>
2023-04-24 14:43:39 -0400

    Add files via upload

---
+++ csci easy quiz/EasyQuiz/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class mainForm
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnScience_Click(sender As Object, e As EventArgs) Handles btnScience.Click
+
+    End Sub
+End Class
